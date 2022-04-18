5ea1ec89d308b074308601117d19c2aa616d9ac2
HomerOts <torresmesurhomero@gmail.com>
2022-04-18 19:33:13 -0300

    inicios

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+let nombre= prompt("Ingrese su nombre");
+let apellido= prompt("Ingrese su apellido");
+
+if ((nombre ==="") && (apellido==="")){
+    alert("No ha ingresado ningún caracter")
+} else ((nombre !==" ") && (apellido !==" "));{
+    alert("Nombre: "+nombre +"\nApellido: "+apellido);
+};
+
+
