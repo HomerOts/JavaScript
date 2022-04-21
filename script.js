331a15fe9849ce7b9442d7f89373a2ddee495ccc
HomerOts <torresmesurhomero@gmail.com>
2022-04-20 23:50:32 -0300

    desafio

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-
 const vuelta = parseFloat(1.20)
 const km = parseInt(40)
 let distancia = parseFloat(prompt("Ingrese el kilometraje de su viaje"))
-
-
 let regreso = prompt("Dicho viaje requiere vuelta?")
+let resultado = parseFloat(distancia * km)
 
-function cotizar (distancia,km) {
+function cotizar(distancia, km) {
     return distancia * km
 }
-
-let resultado = parseFloat (distancia * km)
-
-if ((regreso === "si") ||(regreso === "Si") || (regreso === "SI")) {
-    alert("El valor de su viaje es de $"+(resultado * vuelta))
-} 
-else if ((regreso ==="no") || (regreso ==="No") || (regreso ==="NO")) {
-    alert("El valor de su viaje es de $"+(resultado))
-}
-
-
+    if ((regreso === "si") || (regreso === "Si") || (regreso === "SI")) {
+        alert("El valor de su viaje es de $" + (resultado * vuelta))
+    } else if ((regreso === "no") || (regreso === "No") || (regreso === "NO")) {
+        alert("El valor de su viaje es de $" + (resultado))
+    } 
+    do{
+        cotizar
+    }  while (distancia !=" ")
 
 
